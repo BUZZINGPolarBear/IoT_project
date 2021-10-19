89075cf4dbc307387f465745f06da4b312e9f31a
BUZZINGPolarBear <jjh63360@kau.kr>
2021-10-19 11:51:04 +0900

    Switch code style From index.html to esp32.ino

--- 7주차/실습/Task07-E/Task07-E.ino
+++ 7주차/실습/Task07-E/Task07-E.ino
@@ -35,6 +35,8 @@ const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 3600 * 9; // 3600
 const int daylightOffset_sec = 0;    // 3600
 
+String hourZeroToTwelve = "<label class=\"label\">Hour</label><div class=\"select\"> <select name=\"hour\"> <option>00</option> <option>02</option> <option>03</option> <option>04</option> <option>05</option> <option>06</option> <option>07</option> <option>08</option> <option>09</option> <option>10</option> <option>11</option> <option>12</option> <option>13</option> <option>14</option> <option>15</option> <option>16</option> <option>17</option> <option>18</option> <option>19</option> <option>20</option> <option>21</option> <option>22</option> <option>23</option> </select> </div>";
+String minuteZeroToSixty = "<label class=\"label\">Minute</label><div class=\"select\"> <select name=\"minute\"> <option>00</option> <option>01</option> <option>02</option> <option>03</option> <option>04</option> <option>05</option> <option>06</option> <option>07</option> <option>08</option> <option>09</option> <option>10</option> <option>11</option> <option>12</option> <option>13</option> <option>14</option> <option>15</option> <option>16</option> <option>17</option> <option>18</option> <option>19</option> <option>20</option> <option>21</option> <option>22</option> <option>23</option> <option>24</option> <option>25</option> <option>26</option> <option>27</option> <option>28</option> <option>29</option> <option>30</option> <option>31</option> <option>32</option> <option>33</option> <option>34</option> <option>35</option> <option>36</option> <option>37</option> <option>38</option> <option>39</option> <option>40</option> <option>41</option> <option>42</option> <option>43</option> <option>44</option> <option>45</option> <option>46</option> <option>47</option> <option>48</option> <option>49</option> <option>50</option> <option>51</option> <option>52</option> <option>53</option> <option>54</option> <option>55</option> <option>56</option> <option>57</option> <option>58</option> <option>59</option> </select> </div>";
 String printLocalTime()
 {
   struct tm timeinfo;
@@ -155,7 +157,11 @@ void loop()
               output17State = "off";
               digitalWrite(output17, LOW);
             }
-            
+            else if (header.indexOf("GET /get?hour=", 2) >= 0)
+            {
+              
+            }
+
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
@@ -185,8 +191,9 @@ void loop()
             client.println("</br>Set Alarm");
             client.println("<p>");
             client.println("<form action=\"/get\">");
-
-            client.println("<label class=\"label\"><input name=\"hour\" class=\"input\" type=\"text\" style=\"width: 50px;\">Hour<input name=\"min\" class=\"input\" type=\"text\" style=\"width: 50px;\">Minute</label>");
+        
+            client.println(hourZeroToTwelve);
+            client.println(minuteZeroToSixty);
             client.println("<label class=\"label\">Set music</label>");
             client.println("<textarea name=\"music\" id=\"story\" name=\"story\" rows=\"5\" cols=\"33\">04060604044476764404767644767644040404</textarea><br><input type=\"submit\" value=\"Submit\">");
             client.println("</form><div class=\"control\"></div></p>");
