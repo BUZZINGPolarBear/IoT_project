5ec6ed97457269acc2f708c98165d83d782a87f1
BUZZINGPolarBear <jjh63360@kau.kr>
2021-10-19 12:39:55 +0900

    Get music Data

--- 7주차/실습/Task07-E/Task07-E.ino
+++ 7주차/실습/Task07-E/Task07-E.ino
@@ -40,6 +40,7 @@ String hourZeroToTwelve = "<label class=\"label\">Hour</label><div class=\"selec
 String minuteZeroToSixty = "<label class=\"label\">Minute</label><div class=\"select\"> <select name=\"minute\"> <option>00</option> <option>01</option> <option>02</option> <option>03</option> <option>04</option> <option>05</option> <option>06</option> <option>07</option> <option>08</option> <option>09</option> <option>10</option> <option>11</option> <option>12</option> <option>13</option> <option>14</option> <option>15</option> <option>16</option> <option>17</option> <option>18</option> <option>19</option> <option>20</option> <option>21</option> <option>22</option> <option>23</option> <option>24</option> <option>25</option> <option>26</option> <option>27</option> <option>28</option> <option>29</option> <option>30</option> <option>31</option> <option>32</option> <option>33</option> <option>34</option> <option>35</option> <option>36</option> <option>37</option> <option>38</option> <option>39</option> <option>40</option> <option>41</option> <option>42</option> <option>43</option> <option>44</option> <option>45</option> <option>46</option> <option>47</option> <option>48</option> <option>49</option> <option>50</option> <option>51</option> <option>52</option> <option>53</option> <option>54</option> <option>55</option> <option>56</option> <option>57</option> <option>58</option> <option>59</option> </select> </div>";
 String getHour = "";
 String getMin = "";
+String getMusic = "";
 String printLocalTime()
 {
   struct tm timeinfo;
@@ -164,9 +165,11 @@ void loop()
             if (header.indexOf("GET /get?hour=") >= 0)
             {
               getHour = header.substring(header.indexOf("/get?hour=")+10, header.indexOf("/get?hour=")+12);
-              Serial.println(getHour);
+              Serial.println("Got Hour: " + getHour);
               getMin = header.substring(header.indexOf("&minute=")+8, header.indexOf("&minute=")+10);
-              Serial.println(getMin);
+              Serial.println("Got Min: " + getMin);
+              getMusic = header.substring(header.indexOf("&music=")+7, header.indexOf(" H")-2);
+              Serial.println("Got Music: " + getMusic);
             }
 
             // Display the HTML web page
